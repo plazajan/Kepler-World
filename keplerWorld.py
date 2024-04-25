0db9979d8ac83a414e308142bc0505330001a18a
Jan Plaza <104230083+plazajan@users.noreply.github.com>
2024-04-25 16:29:08 -0400

    Update descriptions

--- keplerWorld.py
+++ keplerWorld.py
@@ -14,6 +14,14 @@ The calculations are done for actual celestial body,
 and the results are scaled down to be displayed in turtle graphics.
 The simulation tests if the resulting orbits and bodies' movements obey the Kepler laws.
 
+Concepts of the methodology of science:
+* Areas of science: experimental/observational, theoretical, computational.
+* Discrete simulation of a continuous process.
+* Qualitative vs. quantitative statements
+  (a planet moves fastest when close to the Sun vs. Kepler's 2nd law).
+* Local effects (of the law of gravity) vs.
+  global properties (elliptical orbits with details specified by Kepler).
+
 The simulation shows orbits to scale, 
 but disregards some details not relevant to Kepler's laws.
 Namely, in this simulation (unlike in reality):
@@ -22,14 +30,6 @@ Namely, in this simulation (unlike in reality):
 * the period in which the turtle completes the orbit is proportional to the
   actual orbital period of the planet, but the scaling factor is not the same
   as for the orbit's size.
-
-Concepts of the methodology of science:
-* Areas of science: experimental/observational, theoretical, computational.
-* Discrete simulation of a continuous process.
-* Qualitative vs. quantitative statements
-  (a planet moves fastest when close to the Sun vs. Kepler's 2nd law).
-* Local effects (of the law of gravity) vs.
-  global properties (elliptical orbits with details specified by Kepler).
 
 A slideshow is also available: Discovering the Mechanics of the Solar System.
 """
