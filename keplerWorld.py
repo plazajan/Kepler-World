42286360d289576b3da0ff2cf2e30bf1fd422622
Jan Plaza <104230083+plazajan@users.noreply.github.com>
2024-04-25 15:28:02 -0400

    Updating description

--- keplerWorld.py
+++ keplerWorld.py
@@ -1,31 +1,32 @@
 """
 Kepler's World
 
-A simple simulation of movements of planets around the Sun.
-It uses a discrete approximation of the real-world continuous effect
-of the Newton's law of gravity (F = GMm/(r^2) on the moving planet.
+A simple simulation of movements of celestial bodies around the Sun
+for the purpose of studying Kepler's laws 
+and how they emerge from Newton's law of gravity.
+Designed for high school students or lower level undergraduate students.
 
-The calculations are done for actual planets,
+The simulation uses a discrete approximation of the real-world continuous effect
+of the Newton's law of gravity (F = GMm/(r^2) on the moving body.
+The calculations are done for actual celestial body,
 and the results are scaled down to be displayed in turtle graphics.
+The simulation tests if the resulting orbits and bodies' movements obey the Kepler laws.
 
-The simulation tests if the resulting orbits and planets' movements
-obey the Kepler laws.
-
-The simulation shows planets' orbits to scale, 
+The simulation shows orbits to scale, 
 but disregards some details not relevant to Kepler's laws.
 Namely, in this simulation (unlike in reality):
 * the orbits of all planets are in the same plane, and
 * the foci of elliptical orbits of all planets are on the same straight line.
 * the period in which the turtle completes the orbit is proportional to the
   actual orbital period of the planet, but the scaling factor is not the same
-  as for orbit's size.
+  as for the orbit's size.
 
-Methodology of science concepts:
-* areas of science: experimental/observational, theoretical, computational.
-* discrete simulation of a continuous process.
-* qualitative vs. quantitative statements
+Concepts of the methodology of science:
+* Areas of science: experimental/observational, theoretical, computational.
+* Discrete simulation of a continuous process.
+* Qualitative vs. quantitative statements
   (a planet moves fastest when close to the Sun vs. Kepler's 2nd law).
-* local effects (of the law of gravity) vs.
+* Local effects (of the law of gravity) vs.
   global properties (elliptical orbits with details specified by Kepler).
 
 A slideshow is also available: Discovering the Mechanics of the Solar System.
