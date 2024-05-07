504b8862110ddd186dba03c1d68dfbb4e02b01fe
Jan Plaza <104230083+plazajan@users.noreply.github.com>
2024-05-07 10:12:44 -0400

    Update keplerWorld.py

--- keplerWorld.py
+++ keplerWorld.py
@@ -6,13 +6,15 @@ for the purpose of studying Kepler's laws
 and how they emerge from Newton's law of gravity.
 Designed for high school students or lower level undergraduate students.
 
-Movements of the eight planets are simulated, as well as some celestial bodes
-made up for the purpose of experiments.
+Movements of the eight planets are simulated, as well as movements of
+some celestial bodes made up for the purpose of experiments.
 The simulation uses a discrete approximation of the real-world continuous effect
-of the Newton's law of gravity (F = GMm/(r^2) on the moving body.
+of the Newton's law of gravity (F = GMm/(r^2) on the moving body,
+taking into account only that body and the Sun (the "two-body problem").
 The calculations are done for actual celestial body,
-and the results are scaled down to be displayed in turtle graphics.
-The simulation tests if the resulting orbits and bodies' movements obey the Kepler laws.
+and the results are scaled down to be visualized in turtle graphics.
+The simulation tests if the resulting orbits and bodies' movements obey
+Kepler's laws.
 
 Concepts of the methodology of science:
 * Areas of science: experimental/observational, theoretical, computational.
@@ -20,16 +22,17 @@ Concepts of the methodology of science:
 * Qualitative vs. quantitative statements
   (a planet moves fastest when close to the Sun vs. Kepler's 2nd law).
 * Local effects (of the law of gravity) vs.
-  global properties (elliptical orbits with details specified by Kepler).
+  global properties (elliptical orbits with Kepler's characteristics).
+* Visualization vs. simulation. 
 
-The simulation shows orbits to scale, 
+The visualization shows orbits to scale, 
 but disregards some details not relevant to Kepler's laws.
 Namely, in this simulation (unlike in reality):
 * the orbits of all planets are in the same plane, and
 * the foci of elliptical orbits of all planets are on the same straight line.
-* the period in which the turtle completes the orbit is proportional to the
-  actual orbital period of the planet, but the scaling factor is not the same
-  as for the orbit's size.
+The period in which the turtle completes the orbit is proportional to the
+actual orbital period of the planet, but the scaling factor is not the same
+as for the orbit's size.
 
 A slideshow is also available: Discovering the Mechanics of the Solar System.
 """
@@ -495,11 +498,10 @@ def sky(skyColor="black", showSun=True):
     screen.screensize(1000,1000)
     screen.title("Kepler's world")
     screen.bgcolor(skyColor)
-    t = Turtle(visible=False)
     if showSun:
+        t = Turtle(visible=False)
         t.dot(10, "white") # SUN at (0,0) and is white.
         print("The Sun is not to scale; it is shown much bigger.")
-    del t
 
 #sky()
 
